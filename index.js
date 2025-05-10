e3815069873d9fa10aa6f05639b3fae483e19589
shebnem29 <shebnem.xasan.29@gmail.com>
2025-05-10 20:13:24 +0300

    update

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const categoriesRoute = require('./routes/categories');
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Dietary Management API');
+});
+
 app.use('/categories', categoriesRoute);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
