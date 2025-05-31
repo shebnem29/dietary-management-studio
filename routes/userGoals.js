eb567573f8d265a97f33d939c2ac24f3607f50f0
shebnem29 <shebnem.xasan.29@gmail.com>
2025-05-31 22:49:12 +0300

    fixes for middleware

--- routes/userGoals.js
+++ routes/userGoals.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db");
-const authMiddleware = require("../middleware/auth");
+const {authMiddleware} = require("../middleware/auth");
 
 router.patch("/weight", authMiddleware, async (req, res) => {
     const userId = req.user.id;
