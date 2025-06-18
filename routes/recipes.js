b4c86568c8d074269178cd901d169815ef673036
Shebnem Mehmed <smehmed@shopmetrics.com>
2025-06-18 11:03:55 +0300

    changed name of the route

--- routes/recipes.js
+++ routes/recipes.js
@@ -118,7 +118,7 @@ router.delete('/:id', authenticateToken, async (req, res) => {
   }
 });
 // GET /dish-types
-router.get('/', authenticateToken, async (req, res) => {
+router.get('/dish-types', authenticateToken, async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM public.dish_types'); 
     console.log('Fetched dish types:', result.rows);
